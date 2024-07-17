f3279f61ab2348d8ff6d7667eed87014d6a67e32
aristides1000 <aristides1000@gmail.com>
2024-07-17 14:10:05 -0400

    Feat: passed 2 test of exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let sum = num1;
+  for(num1 = num1 + 1; num1 <= num2; num1++) {
+    sum = sum + num1;
+  }
+  return sum;
 };
 
 // Do not edit below this line
