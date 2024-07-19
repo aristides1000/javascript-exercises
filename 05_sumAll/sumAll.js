5b8eceb030a35353d9e9fe6a6e48bbb1cae935b5
aristides1000 <aristides1000@gmail.com>
2024-07-19 13:55:33 -0400

    Feat: Finish task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,23 @@
 const sumAll = function(num1, num2) {
-  let sum = num1;
-  for(num1 = num1 + 1; num1 <= num2; num1++) {
-    sum = sum + num1;
+  if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
   }
-  return sum;
+
+  if (Number.isInteger(num1) && Number.isInteger(num2)) {
+    if (num1 > num2) {
+      let aux = num1;
+      num1 = num2;
+      num2 = aux;
+    }
+  
+    let sum = num1;
+    for(num1 = num1 + 1; num1 <= num2; num1++) {
+      sum = sum + num1;
+    }
+    return sum;
+  } else {
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
